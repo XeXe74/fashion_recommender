b77cfde2cb56ddf0591b02248bf31f0635534ef5
XeXe74 <mariotg1000@gmail.com>
2026-04-06 15:25:17 +0200

    Added index in Polyvore dataset needed for implementing the visualizer

---
+++ index_dataset.py
@@ -0,0 +1,9 @@
+from datasets import load_from_disk
+
+ds = load_from_disk("data/polyvore_outfits/data")
+print(f"Total items: {len(ds)}")
+
+# Construct index
+item_id_to_index = {ds[i]["item_ID"]: i for i in range(len(ds))}
+print(f"Index built: {len(item_id_to_index)} entries")
+print(f"Example: {list(item_id_to_index.items())[:3]}")
